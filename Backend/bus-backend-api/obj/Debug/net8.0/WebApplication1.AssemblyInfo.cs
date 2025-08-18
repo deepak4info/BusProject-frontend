3e0d756d0d10fa2e95ea849f2f02f93de53e8631
Deepak Verma <deepakw2d@gmail.com>
2025-08-18 12:40:28 +0530

    check code

--- Backend/bus-backend-api/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Backend/bus-backend-api/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89c6c8590bdbcb21e695d60fd1b29fc0e859a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
